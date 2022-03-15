0c35ddde2a1627e65a87ae0dacad46a5d6f6523d
yeschance <kyc1628@naver.com>
2022-03-15 13:15:41 +0900

    login_layout

--- navigation/LoginStack.tsx
+++ navigation/LoginStack.tsx
@@ -1,28 +1,103 @@
-import React from 'react';
+import {
+  KakaoOAuthToken,
+  KakaoProfile,
+  getProfile as getKakaoProfile,
+  login,
+  logout,
+  unlink,
+} from '@react-native-seoul/kakao-login';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
 
 const NativeStack = createNativeStackNavigator();
 
-const Login = () => (
-  <Container>
-    <TouchableOpacity>
-      <Text>Login</Text>
+function Login() {
+
+  const signInWithKakao = async (): Promise<void> => {
+    const token: KakaoOAuthToken = await login();
+
+    setResult(JSON.stringify(token));
+  };
+
+  const signOutWithKakao = async (): Promise<void> => {
+    const message = await logout();
+
+    setResult(message);
+  };
+
+  const getKakaoProfile = async (): Promise<void> => {
+    const profile: KakaoProfile = await getProfile();
+
+    setResult(JSON.stringify(profile));
+  };
+
+  const unlinkKakao = async (): Promise<void> => {
+    const message = await unlink();
+
+    setResult(message);
+  };
+
+  const onPress = () => signInWithKakao();
+
+  return(
+
+  <View style={styles.container}>
+    <ImageBackground 
+      style={styles.bgImage}
+      source={require("../navigation/img/logo.png")}  //이미지경로
+      resizeMode="center" // 'cover', 'contain', 'stretch', 'repeat', 'center' 중 선택 
+      >
+    </ImageBackground>
+    <TouchableOpacity 
+      style={styles.button}
+      onPress={onPress}
+      >
+      <Image
+        style={styles.kakaoImage}
+        source={require("../navigation/img/kakao_logo.png")}
+        resizeMode="cover"
+      />
+      <Text style={styles.title}>카카오로 시작하기</Text>
     </TouchableOpacity>
-  </Container>
-);
+  </View>
+  )
+  
+};
 
 const LoginStack = () => (
   <NativeStack.Navigator screenOptions={{ headerShown: false }}>
     <NativeStack.Screen name='Login' component={Login} />
   </NativeStack.Navigator>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  bgImage:{width: '100%', height: '50%'},
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    width: 200,
+    height: 50,
+    backgroundColor: "#FEE500",
+  },
+  kakaoImage:{width: 30, height: 30, marginRight:5},
+  title:{
+    color: '#191919',
+    fontSize: 16,
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
+});
 
 export default LoginStack;
