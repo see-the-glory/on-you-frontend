3768c1260e368b8583704183c4c27ba82865b1bd
Cm7 <jinkyu0826@gmail.com>
2022-06-21 15:55:43 +0900

    applied react-redux, login api

--- navigation/LoginStack.tsx
+++ navigation/LoginStack.tsx
@@ -1,15 +1,14 @@
-import {
-  KakaoOAuthToken,
-  getProfile as getKakaoProfile,
-  login,
-} from "@react-native-seoul/kakao-login";
+import { KakaoOAuthToken, getProfile as getKakaoProfile, login as kakaoLogin } from "@react-native-seoul/kakao-login";
 import React, { useState } from "react";
 // import ResultView from './IntroTemp';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import styled from "styled-components/native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
-
+import { createIconSetFromIcoMoon } from "react-native-vector-icons";
+import { useMutation } from "react-query";
+import { CommonApi } from "../api";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
+import { Login } from "../store/actions";
 const Container = styled.View`
   flex: 1;
   justify-content: center;
@@ -44,53 +43,47 @@ const Logo = styled.ImageBackground`
 
 const NativeStack = createNativeStackNavigator();
 
-function Login() {
+function kakaoAuth() {
   const [result, setResult] = useState<string>("");
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const mutation = useMutation(CommonApi.getJWT, {
+    onSuccess: (res) => {
+      // redux 저장
+      dispatch(Login(res.token));
+    },
+    onError: (error) => {
+      console.log("--- Error ---");
+      console.log(error);
+      // Toast Message 출력.
+    },
+  });
 
   const signInWithKakao = async () => {
-    const token: KakaoOAuthToken = await login();
+    const token: KakaoOAuthToken = await kakaoLogin();
 
-    console.log(token);
-
-    let jwtToken = fetch("http://3.39.190.23:8080/login/kakao/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: token.accessToken }),
-    })
-      .then((response) => response.json())
-      .then((resData) => {
-        console.log("--- log ---");
-        console.log(resData.token);
-      });
-
-
-    console.log(token.accessToken);
+    mutation.mutate({ token: token.accessToken });
   };
 
   return (
     <Container>
       {/* <ResultView result={result} /> */}
       <Logo
-        source={require("../navigation/img/logo.png")} //이미지경로
+        source={require("../assets/logo.png")} //이미지경로
         resizeMode="center" // 'cover', 'contain', 'stretch', 'repeat', 'center' 중 선택
       ></Logo>
       <KakaoButton onPress={() => signInWithKakao()}>
-        <KakaoImage
-          source={require("../navigation/img/kakao_logo.png")}
-          resizeMode="cover"
-        />
+        <KakaoImage source={require("../assets/kakao_logo.png")} resizeMode="cover" />
         <Title>카카오로 시작하기</Title>
       </KakaoButton>
     </Container>
   );
 }
 
-
 const LoginStack = () => (
   <NativeStack.Navigator screenOptions={{ headerShown: false }}>
-    <NativeStack.Screen name="Login" component={Login} />
+    <NativeStack.Screen name="kakaoAuth" component={kakaoAuth} />
   </NativeStack.Navigator>
 );
 
