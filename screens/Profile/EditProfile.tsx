8ff418f969eafa8a7fc3cda21b0de52d39a5bc66
yeschance <kyc1628@naver.com>
2022-07-19 00:09:19 +0900

    프로필 수정 - 생년월일 달력 기능
    나의 모임
    - 스와이프시 탈퇴버튼

--- screens/Profile/EditProfile.tsx
+++ screens/Profile/EditProfile.tsx
@@ -3,6 +3,64 @@ import { useTheme } from "react-native-paper";
 import styled from "styled-components/native";
 import { RadioButton } from "react-native-paper";
 import * as ImagePicker from "expo-image-picker";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+Date.prototype.format = function (f) {
+  if (!this.valueOf()) return " ";
+
+  var weekName = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+  var d = this;
+
+  return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function ($1) {
+    switch ($1) {
+      case "yyyy":
+        return d.getFullYear();
+      case "yy":
+        return (d.getFullYear() % 1000).zf(2);
+      case "MM":
+        return (d.getMonth() + 1).zf(2);
+      case "dd":
+        return d.getDate().zf(2);
+      case "E":
+        return weekName[d.getDay()];
+      case "HH":
+        return d.getHours().zf(2);
+      case "hh":
+        return ((h = d.getHours() % 12) ? h : 12).zf(2);
+      case "mm":
+        return d.getMinutes().zf(2);
+      case "ss":
+        return d.getSeconds().zf(2);
+      case "a/p":
+        return d.getHours() < 12 ? "오전" : "오후";
+      default:
+        return $1;
+    }
+  });
+};
+
+String.prototype.string = function (len) {
+  var s = "",
+    i = 0;
+  while (i++ < len) {
+    s += this;
+  }
+  return s;
+};
+String.prototype.zf = function (len) {
+  return "0".string(len - this.length) + this;
+};
+Number.prototype.zf = function (len) {
+  return this.toString().zf(len);
+};
 
 const Container = styled.View`
   flex: 1;
@@ -17,19 +75,25 @@ const LogoWrap = styled.View`
 const LogoBtn = styled.TouchableOpacity``;
 
 const LogoBox = styled.View`
-  width: 70px;
-  height: 70px;
+  width: 85px;
+  height: 85px;
   border-radius: 50px;
   justify-content: center;
   align-items: center;
-  background-color: #000;
+  border: 1px;
+  border-color: rgb(255, 255, 255);
+  background-color: white;
+  box-shadow: 1px 1px 1px gray;
   margin-top: 30px;
 `;
 
 const Logo = styled.Image`
-  width: 100%;
-  height: 100%;
-  border-radius: 15px;
+  width: 80px;
+  height: 80px;
+  border-radius: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
 `;
 
 const ProfileText = styled.Text`
@@ -55,6 +119,8 @@ const Input = styled.TextInput`
   padding-bottom: 5px;
 `;
 
+const TextBtn = styled.TouchableOpacity``;
+
 const CheckWrap = styled.View`
   flex-direction: row;
   border-bottom-width: 1px;
@@ -77,6 +143,25 @@ const EditProfile = () => {
   );
 
   const [checked, setChecked] = React.useState("first");
+
+  const placeholder = "날짜를 입력해주세요";
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [text, onChangeText] = useState("");
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("dateFormat: ", date.format("yyyy/MM/dd"));
+    hideDatePicker();
+    onChangeText(date.format("yyyy/MM/dd"));
+  };
 
   const { colors } = useTheme();
 
@@ -124,7 +209,23 @@ const EditProfile = () => {
       </Form>
       <Form>
         <Title>생년월일</Title>
-        <Input autoCorrect={false} />
+        <TextBtn onPress={showDatePicker}>
+          <Input
+            pointerEvents="none"
+            placeholder={placeholder}
+            placeholderTextColor="#000000"
+            underlineColorAndroid="transparent"
+            editable={false}
+            value={text}
+          />
+          <DateTimePickerModal
+            headerTextIOS={placeholder}
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
+        </TextBtn>
       </Form>
       <Form>
         <Title>연락처</Title>
