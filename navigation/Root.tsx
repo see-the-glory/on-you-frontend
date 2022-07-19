49b409ec3ee56ecd4e2249c3e4da9b549e1071e2
gyu-bin <65651062+gyu-bin@users.noreply.github.com>
2022-07-19 11:13:44 +0900

    Update Root.tsx

--- navigation/Root.tsx
+++ navigation/Root.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Tabs from "./Tabs";
 import ClubCreationStack from "./ClubCreationStack";
-import ClubStack from "./ClubTopTabs";
+import ClubStack from "./ClubStack";
 import HomeStack from "./HomeStack";
 import SearchStack from "./SearchStack";
 
 const Nav = createNativeStackNavigator();
 
 const Root = () => (
-  <Nav.Navigator
-    screenOptions={{
-      presentation: "card",
-      headerShown: false,
-    }}
-  >
-    <Nav.Screen name="Tabs" component={Tabs} />
-    <Nav.Screen name="ClubCreationStack" component={ClubCreationStack} />
-    <Nav.Screen name="ClubStack" component={ClubStack} />
-    <Nav.Screen name="HomeStack" component={HomeStack}/>
-    <Nav.Screen name="SearchStack" component={SearchStack}/>
-  </Nav.Navigator>
+    <Nav.Navigator
+        screenOptions={{
+            presentation: "card",
+            headerShown: false,
+        }}
+    >
+        <Nav.Screen name="Tabs" component={Tabs} />
+        <Nav.Screen name="ClubCreationStack" component={ClubCreationStack} />
+        <Nav.Screen name="ClubStack" component={ClubStack} />
+        <Nav.Screen name="HomeStack" component={HomeStack} />
+        <Nav.Screen name="SearchStack" component={SearchStack}/>
+    </Nav.Navigator>
 );
 export default Root;
