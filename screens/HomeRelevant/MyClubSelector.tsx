921bd20e87dcdc2c3c0b99221943280710ab5a8b
gyubin <rbqls6651@naver.com>
2023-01-10 21:00:11 +0900

    클럽 선택영역 에러 수정

--- screens/HomeRelevant/MyClubSelector.tsx
+++ screens/HomeRelevant/MyClubSelector.tsx
@@ -170,15 +170,7 @@ const MyClubSelector: React.FC<MyClubSelectorScreenProps> = ({ navigation: { nav
                           <CommentRemainder>
                             <CtrgArea>
                               <CtgrText>
-                                {item.categories?.length>1?
-                                  (
-                                    <View style={{display:'flex',flexDirection:'row'}}>
-                                      <OrganizationName>{item.categories?.map((name)=>{return name.name})}</OrganizationName>
-                                    </View>
-                                  ):(
-                                    <OrganizationName>{item.categories[0].name}</OrganizationName>
-                                  )
-                                }
+                                <OrganizationName>{item.categories?.map((name)=>{return name.name})}</OrganizationName>
                               </CtgrText>
                             </CtrgArea>
                           </CommentRemainder>
