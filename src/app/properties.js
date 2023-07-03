26027475b7459c1088a276107ef24dcd0184fdc6
jglee <jinkyu0826@gmail.com>
2023-07-03 12:32:47 +0900

    typo fix

--- src/app/properties.js
+++ src/app/properties.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = "http://125.129.230.5:8080"; //  dev "http://125.129.230.5:8080"; "http://3.39.190.23:8080";
-export const CHAT_BASE_URL = "http://3.39.190.23:8080"; // "http://3.39.190.23:8080";
-export const FILE_BASE_URL = "http://3.39.190.23:8080"; // "http://3.39.190.23:8080";
+export const API_BASE_URL = "http://3.39.190.23:8080";
+export const CHAT_BASE_URL = "http://3.39.190.23:8080";
+export const FILE_BASE_URL = "http://3.39.190.23:8080";
 
 export const IOS_STORE_URL = "itms-apps://apps.apple.com/kr/app/%EC%98%A8%EC%9C%A0/id1663717005";
 export const AOS_STORE_URL = "https://play.google.com/store/apps/details?id=com.onyoufrontend";
